eb7c9e4e4e35ce8e53128a31ae1eb90b9ec935f5
Nalini <bhavitsunkara@gmail.com>
2024-10-31 01:04:03 +0530

    removed SystemInit

--- Objects/main.d
+++ Objects/main.d
@@ -1,5 +1,5 @@
 ./objects/main.o: main.c \
   C:\Keil_v5\l\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  RTE\_stmf103c8t6\RTE_Components.h \
+  RTE\_stm32f1xx\RTE_Components.h \
   C:\Keil_v5\l\Arm\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
   C:\Keil_v5\l\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
