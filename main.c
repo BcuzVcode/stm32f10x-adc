bce35e4a3e1e5cf380426696f6dc7b252cb5394f
Nalini <bhavitsunkara@gmail.com>
2024-10-31 00:05:05 +0530

    fixed adc visibility

--- main.c
+++ main.c
@@ -2,21 +2,10 @@
 #include <stdio.h>
 #include <stdint.h>
 
+volatile uint16_t adc_value_register;
 
 void ADC_Init(void);
 uint16_t ADC_Read(void);
-void initialise_monitor_handles(void);
-int putc(int ch, FILE *f);
-void USART_Init(void);
-
-// Function to initialize semihosting
-void USART_Init(void) {
-    initialise_monitor_handles(); // Initialize semihosting
-}
-void initialise_monitor_handles(void) {
-    // Simple breakpoint for semihosting initialization
-    __asm volatile("bkpt 0xAB");
-}
 
 void ADC_Init(void){
 
@@ -49,45 +38,18 @@ uint16_t ADC_Read(void){
 	
 	while (!(ADC1->SR & ADC_SR_EOC));
 	
-	return ((ADC1->DR) & 0x0FFF);
-}
-// Redirect output to semihosting
-int fputc(int ch, FILE *f) {
-    if (ch == '\n') {
-        fputc('\r', f); // Send carriage return for new lines
-    }
-    
-    // Prepare semihosting call parameters
-    int r0 = 0x3;   // SYS_WRITE (stdout)
-    int r1 = ch;    // Character to write
-    int r2 = 1;     // Number of bytes to write
-
-    // Use inline assembly to call the semihosting function
-    __asm volatile (
-        "mov r0, %0\n"   // Move r0 into the first parameter
-        "mov r1, %1\n"   // Move r1 into the second parameter
-        "mov r2, %2\n"   // Move r2 into the third parameter
-        "bkpt 0xAB"      // Call semihosting function
-        : 
-        : "r"(r0), "r"(r1), "r"(r2)  // Input operands
-        : "r0", "r1", "r2"           // Clobbered registers
-    );
-
-    return ch; // Return character
+	adc_value_register = ((ADC1->DR) & 0x0FFF);
+	
+	return adc_value_register;
 }
 
 int main(void){
 
 	SystemInit();
-	USART_Init();
 	ADC_Init();
-	
-	uint16_t adcValue;
-	
-	while(1){
-		adcValue = ADC_Read();
-		printf("Current ADC Reading is %d", adcValue);
 		
+	while(1){
+		ADC_Read();
 		uint32_t delay_ms = 1000;
 		while(delay_ms--){
 			__NOP();
